021dc817c3ee37ea720645beee55c927e6d037e0
Rudolf Kolbe <rkolbe96@gmail.com>
2023-07-09 20:52:56 +0200

    rewrite tests

--- tests/test.rs
+++ tests/test.rs
@@ -1,9 +1,10 @@
+#![allow(non_snake_case)]
+
 #[cfg(test)]
 mod tests {
-    use std::fs::DirEntry;
-
     // benchmarking
     use std::time::Instant;
+    // path resolving
     use std::{
         fs,
         path::{Path, PathBuf},
@@ -12,159 +13,210 @@ mod tests {
     use texture2ddecoder::*;
     // image saving
     extern crate image;
-    // ktx decoder
-    extern crate ktx2;
-    // dds decoder
+    // texture file decoder
     extern crate ddsfile;
+    extern crate ktx2;
 
-    fn decode_pvrtc_2bpp(data: &[u8], m_width: usize, m_height: usize, image: &mut [u32]) {
-        decode_pvrtc(data, m_width, m_height, image, true);
-    }
-    fn decode_pvrtc_4bpp(data: &[u8], m_width: usize, m_height: usize, image: &mut [u32]) {
-        decode_pvrtc(data, m_width, m_height, image, false);
+    // test functions
+    #[test]
+    fn test_ATC_RGB() {
+        test_format("ATC_RGB", "dds", decode_atc_rgb4)
     }
 
+    #[test]
+    fn test_ATC_RGBA_Explicit() {
+        test_format("ATC_RGBA_Explicit", "dds", decode_atc_rgba8)
+    }
+
+    #[test]
+    fn test_ATC_RGBA_Interpolated() {
+        test_format("ATC_RGBA_Interpolated", "dds", decode_atc_rgba8)
+    }
+
+    #[test]
+    fn test_BC1() {
+        test_format("BC1", "ktx2", decode_bc1)
+    }
+
+    #[test]
+    fn test_BC3() {
+        test_format("BC3", "ktx2", decode_bc3)
+    }
+
+    #[test]
+    fn test_BC4() {
+        test_format("BC4", "ktx2", decode_bc4)
+    }
+
+    #[test]
+    fn test_BC5() {
+        test_format("BC5", "ktx2", decode_bc5)
+    }
+
+    // #[test]
+    // fn test_BC6H() {
+    //     test_format("BC6H", "ktx2", decode_bc6)
+    // }
+
+    // #[test]
+    // fn test_BC7() {
+    //     test_format("BC7", "ktx2", decode_bc7)
+    // }
+
+    #[test]
+    fn test_ETC1_RGB() {
+        test_format("ETC1_RGB", "ktx2", decode_etc1)
+    }
+
+    #[test]
+    fn test_ETC2_RGB() {
+        test_format("ETC2_RGB", "ktx2", decode_etc2)
+    }
+
+    #[test]
+    fn test_ETC2_RGBA() {
+        test_format("ETC2_RGBA", "ktx2", decode_etc2a8)
+    }
+
+    #[test]
+    fn test_ETC2_RGB_A1() {
+        test_format("ETC2_RGB_A1", "ktx2", decode_etc2a1)
+    }
+
+    #[test]
+    fn test_PVRTCI_2bpp_RGB() {
+        test_format("PVRTCI_2bpp_RGB", "ktx2", decode_pvrtc_2bpp)
+    }
+
+    #[test]
+    fn test_PVRTCI_2bpp_RGBA() {
+        test_format("PVRTCI_2bpp_RGBA", "ktx2", decode_pvrtc_2bpp)
+    }
+
+    #[test]
+    fn test_PVRTCI_4bpp_RGB() {
+        test_format("PVRTCI_4bpp_RGB", "ktx2", decode_pvrtc_4bpp)
+    }
+
+    #[test]
+    fn test_PVRTCI_4bpp_RGBA() {
+        test_format("PVRTCI_4bpp_RGBA", "ktx2", decode_pvrtc_4bpp)
+    }
+
+    #[test]
+    fn test_EAC_R11() {
+        test_format("EAC_R11", "ktx2", decode_eacr)
+    }
+
+    #[test]
+    fn test_EAC_RG11() {
+        test_format("EAC_RG11", "ktx2", decode_eacrg)
+    }
+
+    // helper structs and functions
     struct Texture {
         width: u32,
         height: u32,
         data: Vec<u8>,
     }
 
-    #[test]
-    fn main() {
-        let mut d = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
-        d.push("resources/tests/textures");
+    impl Texture {
+        fn new(width: u32, height: u32, data: Vec<u8>) -> Self {
+            Self {
+                width,
+                height,
+                data,
+            }
+        }
 
-        for file in fs::read_dir(d).unwrap() {
-            let entry = file.unwrap();
-            test_file(entry);
+        fn from_file(fp: &str) -> Texture {
+            let extension = Path::new(fp).extension().unwrap().to_str().unwrap();
+            match extension {
+                "ktx2" => Texture::from_ktx2_file(fp),
+                "dds" => Texture::from_dds_file(fp),
+                _ => panic!("Unsupported file format"),
+            }
+        }
+
+        fn from_ktx2_file(fp: &str) -> Texture {
+            let ktx2_data = fs::read(fp).unwrap();
+            let reader = ktx2::Reader::new(ktx2_data).expect("Can't create reader"); // Crate instance of reader.
+
+            // Get general texture information.
+            let header = reader.header();
+
+            // Read iterator over slices of each mipmap level.
+            let levels = reader.levels().collect::<Vec<_>>();
+
+            Texture::new(header.pixel_width, header.pixel_height, levels[0].to_vec())
+        }
+
+        fn from_dds_file(fp: &str) -> Texture {
+            let dds_data = fs::read(fp).unwrap();
+            let dds = ddsfile::Dds::read(&dds_data[..]).unwrap();
+
+            Texture::new(dds.header.width, dds.header.height, dds.data)
+        }
+
+        fn _decode(&self, decode_func: fn(&[u8], usize, usize, &mut [u32])) -> Vec<u32> {
+            let mut image: Vec<u32> = vec![0; (self.width * self.height) as usize];
+            let start = Instant::now();
+            decode_func(
+                &self.data,
+                self.width as usize,
+                self.height as usize,
+                &mut image,
+            );
+            let duration = start.elapsed();
+            println!("Time elapsed in decoding is: {:?}", duration);
+            image
+        }
+        fn save_as_image(&self, path: &str, decode_func: fn(&[u8], usize, usize, &mut [u32])) {
+            let image = self._decode(decode_func);
+            let image_buf = image
+                .iter()
+                .flat_map(|x| {
+                    let v = x.to_le_bytes();
+                    [v[2], v[1], v[0], v[3]]
+                })
+                .collect::<Vec<u8>>();
+
+            image::save_buffer(
+                Path::new(path),
+                &image_buf,
+                self.width,
+                self.height,
+                image::ColorType::Rgba8,
+            )
+            .unwrap();
         }
     }
 
-    fn test_file(entry: DirEntry) {
-        // Crate instance of reader. This validates the header
-        let binding = entry.path();
-        let filename = Path::new(binding.to_str().unwrap()).file_name().unwrap();
+    fn get_texture_fp(name: &str) -> String {
+        let mut d = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+        d.push("resources/tests/textures");
+        d.push(name);
+        d.to_str().unwrap().to_string()
+    }
 
-        println!("Testing {}...", filename.to_str().unwrap());
-
-        let binding = entry.path();
-        let (texture_format, file_format) = filename.to_str().unwrap().split_once('.').unwrap();
-
-        let file_parse_func = get_parse_func(file_format);
-        let texture_decode_func = get_decode_func(texture_format);
-
-        if file_parse_func.is_none() || texture_decode_func.is_none() {
-            println!("... is not supported");
-            return;
-        }
-
-        let texture = file_parse_func.unwrap()(binding.to_str().unwrap());
-
-        let mut image: Vec<u32> = vec![0; texture.width as usize * texture.height as usize];
-
-        let start = Instant::now();
-        texture_decode_func.unwrap()(
-            &texture.data,
-            texture.width as usize,
-            texture.height as usize,
-            &mut image,
-        );
-        let duration = start.elapsed();
-        println!(
-            "{}.{:03} seconds",
-            duration.as_secs(),
-            duration.subsec_millis()
-        );
-
+    fn get_image_fp(name: &str) -> String {
         let mut d = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
         d.push("resources/tests/decompressed");
         fs::create_dir_all(&d).unwrap();
-        d.push(format!("{}.png", texture_format));
-        store_u32_bgra(
-            &image,
-            d.as_path().to_str().unwrap(),
-            texture.width as usize,
-            texture.height as usize,
-        );
+        d.push(name);
+        d.to_str().unwrap().to_string()
     }
 
-    fn get_decode_func(format: &str) -> Option<fn(&[u8], usize, usize, &mut [u32])> {
-        Some(match format {
-            "ATC_RGB" => decode_atc_rgb4,
-            "ATC_RGBA_Explicit" => decode_atc_rgba8,
-            "ATC_RGBA_Interpolated" => decode_atc_rgba8,
-            "BC1" => decode_bc1,
-            "BC3" => decode_bc3,
-            "BC4" => decode_bc4,
-            "BC5" => decode_bc5,
-            "BC6H" => decode_bc6,
-            "BC7" => decode_bc7,
-            "ETC1_RGB" => decode_etc1,
-            "ETC2_RGB" => decode_etc2,
-            "ETC2_RGBA" => decode_etc2a8,
-            "ETC2_RGB_A1" => decode_etc2a1,
-            "PVRTCI_2bpp_RGB" => decode_pvrtc_2bpp,
-            "PVRTCI_2bpp_RGBA" => decode_pvrtc_2bpp,
-            "PVRTCI_4bpp_RGB" => decode_pvrtc_4bpp,
-            "PVRTCI_4bpp_RGBA" => decode_pvrtc_4bpp,
-            "EAC_R11" => decode_eacr,
-            "EAC_RG11" => decode_eacrg,
-            _ => return None,
-        })
-    }
+    fn test_format(
+        name: &str,
+        sample_extension: &str,
+        decode_func: fn(&[u8], usize, usize, &mut [u32]),
+    ) {
+        println!("Testing {}", name);
+        let src_fp = get_texture_fp(&format!("{}.{}", name, sample_extension));
+        let dst_fp = get_image_fp(&format!("{}.png", name));
 
-    fn get_parse_func(extension: &str) -> Option<fn(&str) -> Texture> {
-        Some(match extension {
-            "ktx2" => parse_ktx2,
-            "dds" => parse_dds,
-            _ => return None,
-        })
-    }
-
-    fn parse_ktx2(fp: &str) -> Texture {
-        let ktx2_data = fs::read(fp).unwrap();
-        let reader = ktx2::Reader::new(ktx2_data).expect("Can't create reader"); // Crate instance of reader.
-
-        // Get general texture information.
-        let header = reader.header();
-
-        // Read iterator over slices of each mipmap level.
-        let levels = reader.levels().collect::<Vec<_>>();
-
-        Texture {
-            width: header.pixel_width,
-            height: header.pixel_height,
-            data: levels[0].to_vec(),
-        }
-    }
-
-    fn parse_dds(fp: &str) -> Texture {
-        let dds_data = fs::read(fp).unwrap();
-        let dds = ddsfile::Dds::read(&dds_data[..]).unwrap();
-
-        Texture {
-            width: dds.header.width,
-            height: dds.header.height,
-            data: dds.data,
-        }
-    }
-
-    fn store_u32_bgra(image: &[u32], path: &str, w: usize, h: usize) {
-        let image_buf = image
-            .iter()
-            .flat_map(|x| {
-                let v = x.to_le_bytes();
-                [v[2], v[1], v[0], v[3]]
-            })
-            .collect::<Vec<u8>>();
-        image::save_buffer(
-            Path::new(path),
-            &image_buf,
-            w as u32,
-            h as u32,
-            image::ColorType::Rgba8,
-        )
-        .unwrap();
+        let texture = Texture::from_file(&src_fp);
+        texture.save_as_image(&dst_fp, decode_func);
     }
 }
