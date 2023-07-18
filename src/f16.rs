d42744f24e172e9beb1a0e570c26f00cf5e438ac
Rudolf Kolbe <rkolbe96@gmail.com>
2023-07-18 15:59:07 +0200

    f16 - use correct license information

--- src/f16.rs
+++ src/f16.rs
@@ -1,5 +1,31 @@
-// Helper functions for converting between IEEE single-precision and half-precision floating-point
+/*
+    License Information
 
+    FP16 library is derived from https://github.com/Maratyszcza/FP16.
+    The library is licensed under the MIT License shown below.
+
+
+    The MIT License (MIT)
+
+    Copyright (c) 2017 Facebook Inc.
+    Copyright (c) 2017 Georgia Institute of Technology
+    Copyright 2019 Google LLC
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+    documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
+    rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+    permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all copies or substantial portions of the
+    Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+    WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+    COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+    OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+// Helper functions for Rust for converting between IEEE single-precision and half-precision floating-point
 #[inline]
 const fn __builtin_clz(x: u32) -> u32 {
     let mut ret: u32 = 0;
@@ -26,23 +52,6 @@ fn fp32_from_bits(x: u32) -> f32 {
 fn fp32_to_bits(x: f32) -> u32 {
     u32::from_le_bytes(f32::to_le_bytes(x))
 }
-
-// ported version of AMD's code:
-/*
- * Copyright 2022-2023 Advanced Micro Devices Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
-*/
 
 /// Convert a 16-bit floating-point number in IEEE half-precision format, in bit representation, to
 /// a 32-bit floating-point number in IEEE single-precision format.
